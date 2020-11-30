4e9a2738fcd22162ca7795e91be5e694247e2708
Zoli <toth_zoltan@tolna.net>
2020-11-30 16:38:22 +0100

    Add project files.

---
+++ TransportingSolution/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace TransportingSolution
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSourceNum = new System.Windows.Forms.Label();
+            this.lblDrainNum = new System.Windows.Forms.Label();
+            this.nudSourceNum = new System.Windows.Forms.NumericUpDown();
+            this.nudDrainNum = new System.Windows.Forms.NumericUpDown();
+            this.dgvTransport = new System.Windows.Forms.DataGridView();
+            this.btnCalc = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSourceNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDrainNum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTransport)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSourceNum
+            // 
+            this.lblSourceNum.AutoSize = true;
+            this.lblSourceNum.Location = new System.Drawing.Point(13, 31);
+            this.lblSourceNum.Name = "lblSourceNum";
+            this.lblSourceNum.Size = new System.Drawing.Size(116, 13);
+            this.lblSourceNum.TabIndex = 0;
+            this.lblSourceNum.Text = "Források (sorok) száma";
+            // 
+            // lblDrainNum
+            // 
+            this.lblDrainNum.AutoSize = true;
+            this.lblDrainNum.Location = new System.Drawing.Point(13, 64);
+            this.lblDrainNum.Name = "lblDrainNum";
+            this.lblDrainNum.Size = new System.Drawing.Size(124, 13);
+            this.lblDrainNum.TabIndex = 1;
+            this.lblDrainNum.Text = "Nyelők (oszlopok) száma";
+            // 
+            // nudSourceNum
+            // 
+            this.nudSourceNum.Location = new System.Drawing.Point(149, 29);
+            this.nudSourceNum.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudSourceNum.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudSourceNum.Name = "nudSourceNum";
+            this.nudSourceNum.Size = new System.Drawing.Size(41, 20);
+            this.nudSourceNum.TabIndex = 2;
+            this.nudSourceNum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudSourceNum.ValueChanged += new System.EventHandler(this.nudSourceNum_ValueChanged);
+            // 
+            // nudDrainNum
+            // 
+            this.nudDrainNum.Location = new System.Drawing.Point(149, 62);
+            this.nudDrainNum.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudDrainNum.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudDrainNum.Name = "nudDrainNum";
+            this.nudDrainNum.Size = new System.Drawing.Size(41, 20);
+            this.nudDrainNum.TabIndex = 3;
+            this.nudDrainNum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudDrainNum.ValueChanged += new System.EventHandler(this.nudDrainNum_ValueChanged);
+            // 
+            // dgvTransport
+            // 
+            this.dgvTransport.AllowUserToAddRows = false;
+            this.dgvTransport.AllowUserToDeleteRows = false;
+            this.dgvTransport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTransport.Location = new System.Drawing.Point(16, 140);
+            this.dgvTransport.Name = "dgvTransport";
+            this.dgvTransport.Size = new System.Drawing.Size(848, 305);
+            this.dgvTransport.TabIndex = 4;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(376, 43);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(134, 38);
+            this.btnCalc.TabIndex = 5;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 524);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.dgvTransport);
+            this.Controls.Add(this.nudDrainNum);
+            this.Controls.Add(this.nudSourceNum);
+            this.Controls.Add(this.lblDrainNum);
+            this.Controls.Add(this.lblSourceNum);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudSourceNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDrainNum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTransport)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSourceNum;
+        private System.Windows.Forms.Label lblDrainNum;
+        private System.Windows.Forms.NumericUpDown nudSourceNum;
+        private System.Windows.Forms.NumericUpDown nudDrainNum;
+        private System.Windows.Forms.DataGridView dgvTransport;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ TransportingSolution/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ILOG.Concert;
+using ILOG.CPLEX;
+
+namespace TransportingSolution
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void nudSourceNum_ValueChanged(object sender, EventArgs e)
+        {
+            dgvTransport.RowCount = (int)nudSourceNum.Value + 1;
+            foreach (DataGridViewRow r in dgvTransport.Rows) {
+                r.HeaderCell.Value = "";
+            }
+            if (dgvTransport.RowCount > 2) {
+                DataGridViewRow row = dgvTransport.Rows[dgvTransport.RowCount - 1];
+                row.HeaderCell.Value = "Nyelők (Drains)";
+            }
+        }
+
+        private void nudDrainNum_ValueChanged(object sender, EventArgs e)
+        {
+            dgvTransport.ColumnCount = (int)nudDrainNum.Value + 1;
+            foreach (DataGridViewColumn c in dgvTransport.Columns) {
+                c.HeaderCell.Value = "";
+            }
+
+            if (dgvTransport.RowCount > 2)
+            {
+                DataGridViewColumn column = dgvTransport.Columns[dgvTransport.ColumnCount - 1];
+                column.HeaderCell.Value = "Források (Sources)";
+
+            }
+        }
+
+        private void Calculate() {
+            try {
+
+               
+
+
+                Cplex cplex = new Cplex();
+                float SumSources=0;
+                float SumDrains=0;
+                int sumValue;
+                int Supply = (int)nudSourceNum.Value;
+                int Drain = (int)nudDrainNum.Value;
+                int[][] Cmatrix = new int[Supply][];
+                int[] sources = new int[Supply];
+                int[] drains = new int[Drain];
+                int[] temp;
+                INumVar[][] x = new INumVar[Supply][];
+                INumVar[][] y = new INumVar[Supply][];
+
+                sumValue = 0;
+                for (int i = 0; i < dgvTransport.RowCount - 1; i++)
+                {
+                    int.TryParse(dgvTransport[dgvTransport.ColumnCount - 1, i].Value.ToString(), out sumValue) ;
+                    SumSources += sumValue;
+                }
+
+                sumValue = 0;
+                for (int i = 0; i < dgvTransport.ColumnCount - 1; i++)
+                {
+                    int.TryParse(dgvTransport[i, dgvTransport.RowCount - 1].Value.ToString(), out sumValue);
+                    SumDrains += sumValue;
+                }
+
+                if (SumSources != SumDrains) {
+                    //Nyílt szállítási feladat
+
+                    if (SumSources < SumDrains)
+                    {
+
+                        DataGridViewRow r = new DataGridViewRow();
+                        for (int i = 0; i < dgvTransport.ColumnCount - 2; i++)
+                        {
+                            DataGridViewCell c = new DataGridViewTextBoxCell();
+                            c.Value = 0;
+                            r.Cells.Add(c);
+                        }
+                        DataGridViewTextBoxCell cSupply = new DataGridViewTextBoxCell();
+                        cSupply.Value = SumDrains - SumSources;
+                        dgvTransport.Rows.Insert(dgvTransport.RowCount - 2, r);
+                    }
+                    //egyenlő itt nem lehet, mert csak akkor lép be, ha nem egyenlő a két érték
+                    else {
+                        DataGridViewTextBoxColumn col = new DataGridViewColumn();
+
+                        for (int i = 0; i < dgvTransport.RowCount - 2; i++)
+                        {
+                            DataGridViewCell c = new DataGridViewTextBoxCell();
+                            c.Value = 0;
+                     //       col.Cells.Add(c);
+                        }
+                        DataGridViewTextBoxCell cDrain = new DataGridViewTextBoxCell();
+                        cSupply.Value = SumSources-SumDrains;
+                        dgvTransport.Columns.Insert(dgvTransport.ColumnCount - 2, col);
+
+                    }
+                
+                }
+
+                for (int i = 0; i < (int)nudSourceNum.Value; i++) {
+                    x[i] = cplex.NumVarArray(Drain, 0.0, int.MaxValue);
+                    y[i] = cplex.NumVarArray(Drain, 0.0, int.MaxValue);
+                }
+
+                //források
+
+                for (int i = 0; i < dgvTransport.RowCount - 1; i++) {
+                    int.TryParse(dgvTransport[dgvTransport.ColumnCount - 1, i].Value.ToString(), out sources[i]);
+
+                }
+                //nyelők 
+                for (int i = 0; i < dgvTransport.ColumnCount - 1; i++)
+                {
+                    int.TryParse(dgvTransport[i, dgvTransport.RowCount - 1].Value.ToString(), out drains[i]);
+                }
+
+                //szám van-e mindenhol
+                for (int i = 0; i < dgvTransport.RowCount - 1; i++)
+                {
+                    temp = new int[Drain];
+                    for (int j = 0; j < dgvTransport.ColumnCount - 1; j++)
+                    {
+
+                        int.TryParse(dgvTransport[j, i].Value.ToString(), out temp[j]);
+
+                    }
+                    Cmatrix[i] = temp;
+                }
+
+                for (int i = 0; i < Supply; i++)       // supply must meet demand
+                    cplex.AddEq(cplex.Sum(x[i]), sources[i]);
+
+                for (int j = 0; j < Drain; j++)
+                {     // demand must meet supply
+                    ILinearNumExpr v = cplex.LinearNumExpr();
+                    for (int i = 0; i < Supply; i++)
+                        v.AddTerm(1.0, x[i][j]);
+                    cplex.AddEq(v, drains[j]);
+                }
+
+                ILinearNumExpr expr = cplex.LinearNumExpr();
+                for (int i = 0; i < Supply; ++i)
+                {
+                    for (int j = 0; j < Drain; ++j)
+                    {
+                        expr.AddTerm(x[i][j], Cmatrix[i][j]);
+                    }
+                }
+
+                cplex.AddMinimize(expr);
+
+                cplex.Solve();
+                MessageBox.Show(cplex.GetStatus().ToString());
+                System.Console.WriteLine("Solution status = " + cplex.GetStatus());
+                System.Console.WriteLine(" - Solution: ");
+                for (int i = 0; i < Supply; ++i)
+                {
+                    System.Console.Write("   " + i + ": ");
+                    for (int j = 0; j < Drain; ++j)
+                        System.Console.Write("" + cplex.GetValue(x[i][j]) + "\t");
+                    System.Console.WriteLine();
+                }
+                System.Console.WriteLine("   Cost = " + cplex.ObjValue);
+                cplex.End();
+            }
+            catch (ILOG.Concert.Exception icex) {
+                MessageBox.Show(icex.Message);
+            }
+            }
+
+
+
+
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+    }
+}
